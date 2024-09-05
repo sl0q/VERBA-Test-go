7855dfd59ee407e8abe7a4e3831d1574a282846d
sl0q <shelestov221@gmail.com>
2024-09-05 17:46:56 +0700

    implemented view task route

--- controllers/view_task.go
+++ controllers/view_task.go
@@ -1,9 +1,41 @@
 package controllers
 
 import (
+	"VERBA-Test/database"
+	"VERBA-Test/models"
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func ViewTask(ctx *fiber.Ctx) error {
-	return nil
+	log.Println("Received view task by ID request")
+
+	id, _ := ctx.ParamsInt("id")
+	if id == 0 {
+		log.Println("Error: Failed to query data from DB")
+		return ctx.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": "Задача не найдена",
+		})
+	}
+
+	// query tasks from DB
+	var task models.Task
+	queryResult := database.DB.First(&task, id)
+	if queryResult.Error != nil {
+		log.Println("Error: Failed to query data from DB")
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Проблема на сервере",
+		})
+	}
+
+	log.Println("Operation completed successfully")
+	return ctx.Status(fiber.StatusCreated).JSON(fiber.Map{
+		"id":          task.ID,
+		"title":       task.Title,
+		"description": task.Description,
+		"due_date":    task.Due_date,
+		"created_at":  task.Created_at,
+		"updated_at":  task.Updated_at,
+	})
 }
--- routes/routes.go
+++ routes/routes.go
@@ -10,7 +10,7 @@ func SetUpRoutes(app *fiber.App) {
 	app.Get("/", controllers.Index)
 	app.Post("/tasks", controllers.CreateTask)
 	app.Get("/tasks", controllers.ListTasks)
-	app.Get("/tasks/{id}", controllers.ViewTask)
+	app.Get("/tasks/:id", controllers.ViewTask)
 	app.Put("/tasks/{id}", controllers.UpdateTask)
 	app.Delete("/tasks/{id}", controllers.DeleteTask)
 }
