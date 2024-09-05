9b9031bf5806e779b735bc1fd21846154f5a4893
sl0q <shelestov221@gmail.com>
2024-09-05 17:47:52 +0700

    minor fix to routes by ID

--- routes/routes.go
+++ routes/routes.go
@@ -11,6 +11,6 @@ func SetUpRoutes(app *fiber.App) {
 	app.Post("/tasks", controllers.CreateTask)
 	app.Get("/tasks", controllers.ListTasks)
 	app.Get("/tasks/:id", controllers.ViewTask)
-	app.Put("/tasks/{id}", controllers.UpdateTask)
-	app.Delete("/tasks/{id}", controllers.DeleteTask)
+	app.Put("/tasks/:id", controllers.UpdateTask)
+	app.Delete("/tasks/:id", controllers.DeleteTask)
 }
