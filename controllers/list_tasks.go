61c81cd7a5b288a5550202052aa071579bc80c5e
sl0q <shelestov221@gmail.com>
2024-09-05 17:27:21 +0700

    implemented list_tasks_route

--- controllers/list_tasks.go
+++ controllers/list_tasks.go
@@ -1,9 +1,39 @@
 package controllers
 
 import (
+	"VERBA-Test/database"
+	"VERBA-Test/models"
+
+	"log"
+
 	"github.com/gofiber/fiber/v2"
 )
 
 func ListTasks(ctx *fiber.Ctx) error {
-	return nil
+	log.Println("Received tokens giveaway request")
+
+	// query tasks from DB
+	var tasks []models.Task
+	queryResult := database.DB.Find(&tasks)
+	if queryResult.Error != nil {
+		log.Println("Error: Failed to query data from DB")
+		return ctx.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error": "Проблема на сервере",
+		})
+	}
+
+	// composing responce
+	var responce []fiber.Map
+	for _, task := range tasks {
+		responce = append(responce, fiber.Map{
+			"id":          task.ID,
+			"title":       task.Title,
+			"description": task.Description,
+			"due_date":    task.Due_date,
+			"created_at":  task.Created_at,
+			"updated_at":  task.Updated_at,
+		})
+	}
+
+	return ctx.Status(fiber.StatusCreated).JSON(responce)
 }
